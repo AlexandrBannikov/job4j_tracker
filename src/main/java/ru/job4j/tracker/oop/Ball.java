1aefc0521c9c55c2a13a7f8f47aa86ebc4df4ada
macbook <79824177721@yandex.ru>
2023-02-07 15:21:12 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.oop;
+
+public class Ball {
+
+    public void tryRun(Boolean condition) {
+        if (condition) {
+            System.out.println("Колобок съеден...");
+        } else {
+            System.out.println("Колобок сбежал!)");
+        }
+    }
+}
