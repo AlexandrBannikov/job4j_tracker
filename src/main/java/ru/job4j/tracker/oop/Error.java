95916cb20ff965cf913aa762f8931fcbd670bbd7
macbook <79824177721@yandex.ru>
2023-02-07 16:43:08 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error err1 = new Error();
+        Error err2 = new Error(true, 1, "Error found");
+        Error err3 = new Error(true, 2, "Error not found");
+        err1.printInfo();
+        err2.printInfo();
+        err3.printInfo();
+    }
+}
