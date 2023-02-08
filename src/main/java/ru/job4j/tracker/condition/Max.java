03916749569adc16b14a7b8c77cc793f719974d9
macbook <79824177721@yandex.ru>
2023-02-08 18:46:44 +0500

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/ru/job4j/tracker/condition/Max.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.condition;
+
+public class Max {
+
+    public static int max(int left, int right) {
+        int max = left > right ? left : right;
+        return max;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(third, max(first, second));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int tmp = max(third, fourth);
+        return max(first, second, tmp);
+    }
+
+    public static void main(String[] args) {
+
+        int rsl = max(3, 5);
+        System.out.println(rsl);
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/condition/MaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void when2() {
+        int number = Max.max(3, 4);
+        assertThat(number, is(4));
+    }
+
+    @Test
+    public void when3() {
+        int number = Max.max(1, 3, 7);
+        assertThat(number, is(7));
+    }
+
+    @Test
+    public void when4() {
+        int number = Max.max(1, 3, 6, 9);
+        assertThat(number, is(9));
+    }
+}
