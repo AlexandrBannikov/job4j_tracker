f53e08e42826fa11918ac312f90c072ed3b88da6
macbook <79824177721@yandex.ru>
2023-02-10 19:55:31 +0500

    0. Что такое инкапсуляция. [#3818 #390282]

--- src/main/java/ru/job4j/tracker/encapsulation/Config.java
+++ src/main/java/ru/job4j/tracker/encapsulation/Config.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker.encapsulation;
 
 public class Config {
+
     private String name;
     private int position;
     private String[] properties;
@@ -13,27 +14,11 @@ public class Config {
         System.out.println(position);
     }
 
-    public String getName() {
-        return name;
+    public String getProperty(String key) {
+        return search(key);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public String[] getProperties() {
-        return properties;
-    }
-
-    public void setProperties(String[] properties) {
-        this.properties = properties;
+    private String search(String key) {
+        return key;
     }
 }
