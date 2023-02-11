a6b2982762975557daf5f16395383b3dc78211d7
macbook <79824177721@yandex.ru>
2023-02-11 16:00:38 +0500

    3.1. Массив с пустыми ячейками [#176470]

--- src/main/java/ru/job4j/tracker/pojo/Store.java
+++ src/main/java/ru/job4j/tracker/pojo/Store.java
@@ -5,22 +5,27 @@ public class Store {
         Product milk = new Product("Milk", 10);
         Product bread = new Product("Bread", 4);
         Product egg = new Product("Egg", 19);
-        Product[] prods = new Product[3];
+        Product[] prods = new Product[5];
         prods[0] = milk;
         prods[1] = bread;
         prods[2] = egg;
+
         for (Product pr : prods) {
-            System.out.println(pr.getName() + " " + pr.getCount());
+            if (pr != null) {
+                System.out.println(pr.getName() + " - " + pr.getCount());
+            }
         }
         System.out.println("Replace milk to oil.");
         Product oil = new Product("Oil", 11);
         prods[0] = oil;
         for (Product pr : prods) {
-            System.out.println(pr.getName() + " " + pr.getCount());
+            if (pr != null) {
+                System.out.println(pr.getName() + " " + pr.getCount());
+            }
         }
         System.out.println("Show only product > 10");
         for (Product pr : prods) {
-            if (pr.getCount() > 10) {
+            if (pr != null && pr.getCount() > 10) {
                 System.out.println(pr.getName() + " " + pr.getCount());
             }
         }
