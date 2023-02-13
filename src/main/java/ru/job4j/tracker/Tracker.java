9746c87eaf50e8007d649955641984dfec9a3749
macbook <79824177721@yandex.ru>
2023-02-13 16:06:57 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,15 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
+/**
+ * В классе Tracker должны быть методы
+ * добавление заявок - public Item add(Item item);
+ * получение списка всех заявок - public Item[] findAll();
+ * получение списка по имени - public Item[] findByName(String key);
+ * получение заявки по id - public Item findById(int id);
+ */
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +19,18 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                rsl[size++] = item;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
     }
 
     public Item findById(int id) {
@@ -21,5 +43,17 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int copySize = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = this.items[i];
+            if (name.getName().equals(key)) {
+                rsl[copySize++] = name;
+            }
+        }
+        return  Arrays.copyOf(rsl, copySize);
     }
 }
