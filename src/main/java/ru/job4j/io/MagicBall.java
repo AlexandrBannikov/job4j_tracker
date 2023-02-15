48c578747f12e5e731d989fcd55f00faf28f07f3
macbook <79824177721@yandex.ru>
2023-02-15 14:48:08 +0500

    2.0. Чтение из консоли. Класс Scanner. [#179514 #391647]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -13,15 +13,9 @@ public class MagicBall {
         System.out.println(string);
         int answer = new Random().nextInt(3);
         switch (answer) {
-            case 0:
-            System.out.println("Да");
-            break;
-            case 1:
-            System.out.println("Нет");
-            break;
-            default:
-            System.out.println("Может быть");
-            break;
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
         }
     }
 }
