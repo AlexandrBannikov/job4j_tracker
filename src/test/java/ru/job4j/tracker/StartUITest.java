b6354db775f6d63348f6e97d74a34de84aa8e94f
macbook <79824177721@yandex.ru>
2023-02-16 20:58:32 +0500

    8. Input и полиморфизм. [#181094]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
+
     @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC"};
