9764a6b9859ae79c1de9bb6bedd484f348c2b998
macbook <79824177721@yandex.ru>
2023-02-17 12:02:02 +0500

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,40 +2,4 @@ package ru.job4j.tracker;
 
 public class StartUITest {
 
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName()).isEqualTo(expected.getName());
-//    }
-//
-//    @Test
-//    public void whenEditItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("name");
-//        tracker.add(item);
-//        String[] answers = {
-//                String.valueOf(item.getId()), "edited item"
-//        };
-//        StartUI.replaceItem(new StubInput(answers), tracker);
-//        Item edited = tracker.findById(item.getId());
-//        assertThat(edited.getName()).isEqualTo("edited item");
-//    }
-//
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("name");
-//        tracker.add(item);
-//        String[] answers = {
-//                String.valueOf(item.getId())
-//        };
-//        StartUI.deleteItem(new StubInput(answers), tracker);
-//        Item deleteItem = tracker.findById(item.getId());
-//        assertThat(deleteItem).isNull();
-//    }
 }
