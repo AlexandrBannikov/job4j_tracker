15506a2e436b43388f06defc92147ba8d77ccda2
macbook <79824177721@yandex.ru>
2023-02-20 14:01:25 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/tracker/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.ex;
+
+public class Fact {
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must not be less than 0");
+        }
+        int result = 1;
+        for (int index = 2; index <= n; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+}
