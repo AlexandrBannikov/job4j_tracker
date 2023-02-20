d22b6ec91ef59596235f402f11fca7a2d884689b
macbook <79824177721@yandex.ru>
2023-02-20 14:18:30 +0500

    0.3. Тестирование исключений с junit [#219366 #393430]

---
+++ src/test/java/ru/job4j/tracker/ex/FactTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Fact.calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("n must not be less than 0");
+    }
+
+    @Test
+    public void when4To24() {
+        int number = 4;
+        int result = Fact.calc(number);
+        int expected = 24;
+        assertThat(result).isEqualTo(expected);
+    }
+}
