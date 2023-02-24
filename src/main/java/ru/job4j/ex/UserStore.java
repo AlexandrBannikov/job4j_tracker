5e34e2f448164a5d1473a15371960484f4922dda
macbook <79824177721@yandex.ru>
2023-02-24 19:05:46 +0500

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -16,7 +16,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if ((user.getUsername().length() < 3) || !(user.isValid())) {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
             throw new UserInvalidException("User isn`t valid");
         }
         return true;
