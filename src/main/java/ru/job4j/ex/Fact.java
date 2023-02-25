f6dd346e43587110f0445af7dab9b8b037e1cc32
macbook <79824177721@yandex.ru>
2023-02-25 12:28:04 +0500

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #394763]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,17 +2,17 @@ package ru.job4j.ex;
 
 public class Fact {
     public static int calc(int n) {
-        int rsl;
-        if (n == 0 || n == 1) {
-            rsl =  1;
-        } else {
-            rsl = calc(n - 1) * n;
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("n must not be less than 0");
+        }
+        for (int i = 2; i <= n; i++) {
+            rsl *= i;
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-        int rsl = calc(3);
-        System.out.println(rsl);
+        new Fact().calc(-1);
     }
 }
