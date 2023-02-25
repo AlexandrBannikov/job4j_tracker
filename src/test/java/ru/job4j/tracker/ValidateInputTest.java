e9edde58fc18aded38596a28c94494b9976042e7
macbook <79824177721@yandex.ru>
2023-02-25 21:19:19 +0500

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int select = input.askInt("Enter menu:");
+        assertThat(select).isEqualTo(1);
+    }
+
+    @Test
+    public void whenInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int select = input.askInt("Enter menu:");
+        assertThat(select).isEqualTo(1);
+    }
+
+    @Test
+    public void whenMinusSelect() {
+        Output out = new StubOutput();
+        Input input = new StubInput(new String[] {"-3"});
+        ValidateInput in = new ValidateInput(out, input);
+        int select = in.askInt("Enter menu:");
+        assertThat(select).isEqualTo(-3);
+    }
+
+    @Test
+    public void whenALotInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"1", "3"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(3);
+    }
+}
