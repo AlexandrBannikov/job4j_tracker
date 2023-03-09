39e5f336a14806e8bd6e911fc29a5838e5106a43
macbook <79824177721@yandex.ru>
2023-03-09 10:41:36 +0500

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PriorityQueueTest {
+
+    @Test
+    void whenHigherPrioritySecond() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc()).isEqualTo("urgent");
+    }
+}
