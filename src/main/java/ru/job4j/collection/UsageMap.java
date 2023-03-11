a10c02e1d4d092f670b10a30acdae9ef4b6ffe9d
macbook <79824177721@yandex.ru>
2023-03-11 14:37:18 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("79324177721@mail.ru", "Alexandr Alexandrovich Bannikov");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
