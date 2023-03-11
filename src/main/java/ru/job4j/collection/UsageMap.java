e0ba46c6015aeff94315bea46e6eea64599b6c22
macbook <79824177721@yandex.ru>
2023-03-11 19:39:10 +0500

    0. Map, HashMap [#212652 #399453]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,14 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("79324177721@mail.ru", "Alexandr Alexandrovich Bannikov");
+        map.put("79324177721@mail.ru", "Alexandr Alexandrovich Bannikov");
 
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+        System.out.println();
+        map.put("79324177721@mail.ru", "Alexandr  Bannikov");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
