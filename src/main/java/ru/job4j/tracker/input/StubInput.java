d0e455aad82fef18570a240f495aca46024df8b0
Alexandr Bannikov <7932417721@mail.ru>
2024-12-24 11:52:22 +0500

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/tracker/input/StubInput.java
+++ src/main/java/ru/job4j/tracker/input/StubInput.java
@@ -1,8 +1,8 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.input;
 
 public class StubInput implements Input {
 
-    private String[] answers;
+    private final String[] answers;
     private int position = 0;
 
     public StubInput(String[] answers) {
--- src/main/java/ru/job4j/tracker/input/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/input/ValidateInput.java
@@ -1,4 +1,6 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.input;
+
+import ru.job4j.tracker.output.Output;
 
 /**
  * 2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]
