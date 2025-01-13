ced119ad1612641065df91c12a1e894b21751516
Alexandr Bannikov <7932417721@mail.ru>
2025-01-13 10:09:48 +0500

    9. Лямбда выражения и Collection Framework [#505026 #528313]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -30,6 +30,7 @@ public class AnalyzeByMap {
         return list;
     }
 
+    // метод собран с помощью merge
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         Map<String, Integer> map = new LinkedHashMap<>();
         List<Label> list = new ArrayList<>();
@@ -61,6 +62,7 @@ public class AnalyzeByMap {
         return list.get(list.size() - 1);
     }
 
+    // метод собран с помощью merge
     public static Label bestSubject(List<Pupil> pupils) {
         Map<String, Integer> map = new LinkedHashMap<>();
         List<Label> list = new ArrayList<>();
